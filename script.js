b17a28c76ec61c15b610aff2da3f2886151c2b45
Raymond Chavez <94435919+raymondchavez333@users.noreply.github.com>
2026-02-03 16:14:02 +0800

    Remov the logic for playing 5 rounds

--- script.js
+++ script.js
@@ -80,25 +80,25 @@ function playGame(){
 
         
     }
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    playRound(humanSelection,computerSelection);
+    // const humanSelection = getHumanChoice();
+    // const computerSelection = getComputerChoice();
+    // playRound(humanSelection,computerSelection);
     
-    const humanSelection2 = getHumanChoice();
-    const computerSelection2 = getComputerChoice();
-    playRound(humanSelection2,computerSelection2);
+    // const humanSelection2 = getHumanChoice();
+    // const computerSelection2 = getComputerChoice();
+    // playRound(humanSelection2,computerSelection2);
 
-    const humanSelection3 = getHumanChoice();
-    const computerSelection3 = getComputerChoice();
-    playRound(humanSelection3,computerSelection3);
+    // const humanSelection3 = getHumanChoice();
+    // const computerSelection3 = getComputerChoice();
+    // playRound(humanSelection3,computerSelection3);
 
-    const humanSelection4 = getHumanChoice();
-    const computerSelection4 = getComputerChoice();
-    playRound(humanSelection4,computerSelection4);
+    // const humanSelection4 = getHumanChoice();
+    // const computerSelection4 = getComputerChoice();
+    // playRound(humanSelection4,computerSelection4);
 
-    const humanSelection5 = getHumanChoice();
-    const computerSelection5 = getComputerChoice();
-    playRound(humanSelection5,computerSelection5);
+    // const humanSelection5 = getHumanChoice();
+    // const computerSelection5 = getComputerChoice();
+    // playRound(humanSelection5,computerSelection5);
 
 }
 
